89e1740e962e8495ef92a026ae609fd88fde423e
unknown <liethdallal@gmail.com>
2024-02-16 14:07:23 -0600

    fetching all and displaying all workouts fully operational

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,31 @@
-function Home(){
-    return (
-        <div>
-            <h2>Home</h2>
+import { useEffect, useState } from "react"
 
+// components
+import WorkoutDetails from '../components/WorkoutDetails'
+
+
+function Home(){
+    const [workouts, setWorkouts] = useState(null)
+
+    useEffect(() =>{
+        const fetchWorkouts = async () =>{
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
+
+            if(response.ok){
+                setWorkouts(json)
+            }
+        }
+        fetchWorkouts()
+    }, [])
+
+    return (
+        <div className="home">
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <WorkoutDetails key = {workout._id} workout={workout} />
+                    ))}
+            </div>
         </div>
     )
 }
