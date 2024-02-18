73b82ba657e14b5c9dc8291d694c4a69599027b7
unknown <liethdallal@gmail.com>
2024-02-18 10:56:54 -0600

    created a hook for react context and now adding a new workout does not need the page to refresh to pop up

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react"
-
+import { useEffect} from "react"
+import { useWorkoutsContext } from "../hooks/UseWorkoutsContext"
+ 
 // components
 import WorkoutDetails from '../components/WorkoutDetails'
 import WorkoutForm from '../components/WorkoutForm'
@@ -7,7 +8,7 @@ import WorkoutForm from '../components/WorkoutForm'
 
 
 function Home(){
-    const [workouts, setWorkouts] = useState(null)
+    const {workouts, dispatch} = useWorkoutsContext()
 
     useEffect(() =>{
         const fetchWorkouts = async () =>{
@@ -15,11 +16,11 @@ function Home(){
             const json = await response.json()
 
             if(response.ok){
-                setWorkouts(json)
+                dispatch({type: 'SET_WORKOUTS', payload: json})
             }
         }
         fetchWorkouts()
-    }, [])
+    }, [dispatch])
 
     return (
         <div className="home">
